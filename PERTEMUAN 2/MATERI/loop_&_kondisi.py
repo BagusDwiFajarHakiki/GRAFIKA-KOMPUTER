a52e935d7252fde1c7eefa72e0be68c9fdd5dcb7
BagusDwi <114036813+BagusDwiFajarHakiki@users.noreply.github.com>
2025-10-22 14:10:25 +0700

    Add files via upload

---
+++ PERTEMUAN 2/MATERI/loop_&_kondisi.py
@@ -0,0 +1,12 @@
+print("program loop")
+
+x = 50
+if x > 0:
+    print("Titik berada di sisi kanan sumbu Y")
+else:
+    print("Titik berada di sisi kiri sumbu X")
+
+print("\nprogram kondisi")
+
+for i in range(1, 5):
+    print("Titik ke-", i)
