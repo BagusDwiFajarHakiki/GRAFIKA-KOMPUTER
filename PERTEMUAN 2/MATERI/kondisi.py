94107dac778a8531dfae3ca50f46c538ca8dc929
BagusDwi <114036813+BagusDwiFajarHakiki@users.noreply.github.com>
2025-11-03 15:26:35 +0700

    Add files via upload

---
+++ PERTEMUAN 2/MATERI/kondisi.py
@@ -0,0 +1,7 @@
+print("program kondisi")
+
+x = 50
+if x > 0:
+    print("Titik berada di sisi kanan sumbu Y")
+else:
+    print("Titik berada di sisi kiri sumbu X")
---
+++ PERTEMUAN 2/MATERI/loop.py
@@ -0,0 +1,4 @@
+print("\nprogram loop")
+
+for i in range(1, 5):
+    print("Titik ke-", i)
